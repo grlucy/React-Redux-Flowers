eeaa41f93a9d301f6a9b1eac5cc342fbea8cc801
unknown <53538316+grlucy@users.noreply.github.com>
2020-06-29 17:58:26 -0400

    fixed env and created state/effect in Blue page

--- src/pages/Blue/index.js
+++ src/pages/Blue/index.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../utils/API";
 
 function Blue() {
+  const [blueFlowers, setBlueFlowers] = useState([]);
+
+  useEffect(() => {
+    API.search("blue")
+      .then((res) => {
+        setBlueFlowers(res);
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <p>This is Blue</p>
--- src/utils/API.js
+++ src/utils/API.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 export default {
   search: function (color) {
     return axios.get(
-      `https://pixabay.com/api/?key=${process.env.PIXABAY_KEY}&q=${color}+flower+macro&image_type=photo&orientation=horizontal&editors_choice=true&safesearch=true&per_page=6`
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_KEY}&q=${color}+flower+macro&image_type=photo&orientation=horizontal&editors_choice=true&safesearch=true&per_page=6`
     );
   },
 };
