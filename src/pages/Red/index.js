74a36818b3195885b85c35d9e25c7067a5a67197
unknown <53538316+grlucy@users.noreply.github.com>
2020-06-30 12:29:58 -0400

    added components and API call to Red page

--- src/pages/Red/index.js
+++ src/pages/Red/index.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Image from "../../components/Image";
+import ImageContainer from "../../components/ImageContainer";
+import API from "../../utils/API";
 
 function Red() {
+  const [redFlowers, setRedFlowers] = useState([]);
+
+  useEffect(() => {
+    API.search("red")
+      .then((res) => {
+        setRedFlowers(res.data.hits);
+        console.log(res.data.hits);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <>
-      <p>This is Red</p>
-    </>
+    <ImageContainer>
+      {redFlowers.map((flower) => (
+        <Image
+          key={flower.id}
+          previewURL={flower.previewURL}
+          tags={flower.tags}
+        />
+      ))}
+    </ImageContainer>
   );
 }
 
